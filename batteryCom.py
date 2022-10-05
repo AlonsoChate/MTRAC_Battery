8fcf400682902eb66a2d97caf055a8134c490bec
Yiteng <alonsoch@umich.edu>
2022-10-05 16:36:44 -0400

    initial

---
+++ batteryCom.py
@@ -0,0 +1,65 @@
+from serialUtility import serUtil
+from config import inst
+
+class batteryModule:
+    def __init__(self):
+        self.serial = serUtil('/dev/ttyS5', 612500)
+        # volt & temp
+        self.cellVolt = [0] * 6
+        self.moduleVolt = 0
+        self.temperature = [0] * 2
+        self.moduleAddr = 0
+        # status
+        self.alerts = 0
+        self.faults = 0
+        self.COVFaults = 0
+        self.CUVFaults = 0
+
+    def readStatus(self):
+        # read any alerts or faults
+        addr = self.moduleAddr << 1
+        command = addr + bytes([inst['REG_ALERT_STATUS']]) + bytes([0x04])
+        response = self.serial.query(command, 7, False)
+        self.alerts = response[3]
+        self.faults = response[4]
+        self.COVFaults = response[5]
+        self.CUVFaults = response[6]
+
+    def readValues(self):
+        # read temperature and voltage
+        addr = bytes([self.moduleAddr << 1])
+        self.readStatus()
+
+        # ADC Auto mode, read every ADC input we can (Both Temps, Pack, 6 cells)
+        command = addr + bytes([inst['REG_ADC_CTRL']]) + bytes([0b00111101])
+        self.serial.query(command, 3, True)
+
+        # enable temperature measurement VSS pins
+        command = addr + bytes([inst['REG_TO_CTRL']]) + bytes([0b00000011])
+        self.serial.query(command, 3, True)
+
+        # start all ADC conversions
+        command = addr + bytes([inst['REG_ADC_CONV']]) + bytes([0b1])
+        self.serial.query(command, 3, True)
+
+        # start reading registers at the module voltage registers
+        # read 18 bytes
+        # 2 bytes each for ModuleV, CellV 1-6, Temp1, Temp2
+        command = addr + bytes([inst['REG_GPAI']]) + bytes([0x12]) 
+        response = self.serial.query(command, 22, False)
+
+        # response bytes composed of:
+        # 0: address
+        # 1: command
+        # 2: requested number of values
+        # 3-20: value
+        # 21: CRC check
+        CRC = self.serial.genCRC(response[:-1])
+        if response[0] == addr and response[1] == inst['REG_GPAI'] \
+        and response[3] == 0x12 and response[4] == CRC:
+            # TODO:
+            pass
+    
+    def balance(self):
+        # TODO:
+        pass
---
+++ serialUtility.py
@@ -0,0 +1,67 @@
+from time import sleep
+from serial import Serial
+
+class serUtil:
+    def __init__(self, portname, baudrate=9600, readTimeOut=None, writeTimeOut=None):
+        self.portname = portname
+        self.baudrate = baudrate
+        self.readTimeOut = readTimeOut
+        self.writeTimeOut = writeTimeOut
+        # without timeout read or write will block
+        self.ser = Serial(port=portname, baudrate=baudrate, 
+            timeout=readTimeOut, write_timeout=writeTimeOut)
+
+    def __str__(self) -> str:
+        return self.__portname__
+
+    def __del__(self):
+        self.ser.close()
+    
+    def genCRC(self, input: bytes):
+        # generate CRC as bytes type
+        generator = 0x07
+        crc = 0
+        # mask is crutial to make crc uint8
+        mask = 0xFF
+        for byte in input:
+            crc ^= byte
+            crc &= mask
+            for _ in range(8):
+                if (crc & 0x80) != 0:
+                    crc = (crc << 1) & mask ^ generator
+                else:
+                    crc = crc << 1 & mask
+        return bytes([crc & mask])
+
+    def sendCommand(self, command: bytes, withCRC=False):
+        # if withCRC, send an entra byte of CRC
+        # return total number of bytes send        
+        addr = command[0]
+        rest = command[1:]
+        numBytes = 0
+        if withCRC:
+            addr |= 1
+        numBytes += self.ser.write(bytes([addr]))
+        numBytes += self.ser.write(rest)
+        if withCRC:
+            crc = self.genCRC(command)
+            numBytes += self.ser.write(crc)
+        return numBytes
+
+    def getResponse(self, maxLen: int):
+        # clear input buffer when maxLen bytes are read
+        response = self.ser.read(maxLen)
+        self.ser.reset_input_buffer()
+        return response
+
+    def query(self, command: bytes, retLen, withCRC=False):
+        # send command and get response
+        for _ in range(5):
+            # give 5 attemps
+            self.sendCommand(command, withCRC)
+            # TODO test delay time later
+            # sleep(2 * (retLen / 8) + 1)
+            response = self.getResponse(retLen)
+            if len(response) == retLen:
+                return response 
+        return response
